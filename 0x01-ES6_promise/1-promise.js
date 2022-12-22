d63faec5307195cd5e3a19f4b2027b5bbc2dcdfe
melakualehegn <melakualehegn34@gmail.com>
2022-12-22 02:50:28 -0800

    Fake api reject message fix

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -2,10 +2,6 @@ export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
       resolve({ status: 200, body: 'Success' });
-    } else
-      reject("The fake API is not working currently");
+    } else reject(new Error('The fake API is not working currently'));
   });
 }
-getFullResponseFromAPI()
-.then((success) => console.log(success))
-.catch( e => console.error(e) );
