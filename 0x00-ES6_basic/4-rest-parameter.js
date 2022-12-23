8548e649e8c6233df3a0ce6e93ed04e21d22dbba
melakualehegn <melakualehegn34@gmail.com>
2022-12-23 07:20:29 -0800

    Rest parameter syntax for functions eslint fix

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,5 +1,5 @@
 export default function returnHowManyArguments(...args) {
-    let count = 0;
-    for (let arg = 0; arg<args.length; arg++) count++
-    return count
+  let count = 0;
+  for (let arg = 0; arg < args.length; arg += 1) count += 1;
+  return count;
 }
