e21e1fafa2467b1c9bdde80ee7f23cf28b99c3d9
melakualehegn <melakualehegn34@gmail.com>
2022-12-23 12:22:35 -0800

    Parameter defaults

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,6 @@
+export default function appendToEachArrayValue(array, appendString) {
+  for (const [key, value] of array.entries()) {
+    array[key] = `${appendString} ${value}`;
+  }
+  return array;
+}
