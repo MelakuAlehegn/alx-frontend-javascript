7051528852fdf22be8b9c9c0c43274aaf4933ae9
melakualehegn <melakualehegn34@gmail.com>
2023-01-03 10:16:16 +0300

    Handle multiple promises:eslint

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,13 +1,13 @@
-import { signUpUser } from "./4-user-promise";
-import { uploadPhoto } from "./5-photo-reject.js";
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName){
-    return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+export default function handleProfileSignup() {
+  return Promise.allSettled([signUpUser, uploadPhoto])
     .then((result) => {
-        const output = []
-        for (const item of result){
-            output.push({status: item.status, value: item.value})
-        }
-        return output
-    })
+      const output = [];
+      for (const item of result) {
+        output.push({ status: item.status, value: item.value });
+      }
+      return output;
+    });
 }
