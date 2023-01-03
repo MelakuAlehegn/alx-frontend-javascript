4676f4d7aba0b90322ab9e65315b379b1e1e12a2
melakualehegn <melakualehegn34@gmail.com>
2023-01-03 13:26:36 +0300

    Filter: eslint fix

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,3 @@
-export default function getStudentsByLocation(students, city){
-    return students.filter(stud => (stud.location == city))
+export default function getStudentsByLocation(students, city) {
+  return students.filter((stud) => (stud.location === city));
 }
