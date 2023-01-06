28b74895fc64f7c2bca1551840e10998fd15e1d0
melakualehegn <melakualehegn34@gmail.com>
2023-01-06 12:44:06 +0300

    task-4:classes

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -11,6 +11,7 @@ interface Teacher {
 interface Directors extends Teacher {
     numberOfReports: number;
 }
+
 const director1: Directors = {
   firstName: 'John',
   lastName: 'Doe',
@@ -19,3 +20,43 @@ const director1: Directors = {
   numberOfReports: 17,
 };
 console.log(director1);
+
+// eslint-disable-next-line @typescript-eslint/class-name-casing
+interface printTeacherFunction {
+    firstName: string;
+    lastName: string;
+}
+
+function printTeacher(input: printTeacherFunction) : string {
+    return `${input.firstName[0]}. ${input.lastName}`
+}
+
+const teacher: printTeacherFunction = {
+    firstName: "John",
+    lastName: "Doe"
+}
+
+console.log(printTeacher(teacher));
+
+interface studentInterface{
+    firstName:string;
+    lastName:string;
+    workOnHomework(): string;
+    displayName():string;
+}
+
+class StudentClass implements studentInterface{
+    firstName: string;
+    lastName:string;
+    constructor(firstName:string, lastName:string;){
+       this.firstName = firstName,
+       this.lastName = lastName
+    }
+    workOnHomework(): string {
+        return "Currently working"
+    }
+    displayName(): string {
+        return this.firstName
+    }
+}
+console.log(StudentClass())
