fe2cf07e365a20aaf4c969ac7049f2f4933f10fd
melakualehegn <melakualehegn34@gmail.com>
2023-01-09 15:39:03 +0300

    task six completed

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -2,7 +2,7 @@ export default function createInt8TypedArray(length, position, value) {
   if (position >= length) {
     throw new Error('Position outside range');
   }
-  const buf = new DataView(new ArrayBuffer(length), 0, length);
-  buf.setInt8(position, value);
-  return buf;
+  const myarry = new DataView(new ArrayBuffer(length), 0, length);
+  myarry.setInt8(position, value);
+  return myarry;
 }
