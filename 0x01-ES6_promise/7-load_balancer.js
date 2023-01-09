294e9033ce38835d092b9a16e2b3d71b5e48dfad
melakualehegn <melakualehegn34@gmail.com>
2023-01-09 10:36:14 +0300

    Load balancer

---
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -0,0 +1,5 @@
+export default function loadBalancer(chinaDownload, USDownload) {
+  const china = chinaDownload();
+  const US = USDownload();
+  return Promise.race([china, US]).then((val) => console.log(val), (err) => console.log(err));
+}
---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,61 @@
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    [index: string]: any;
+}
+
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
+
+// eslint-disable-next-line @typescript-eslint/class-name-casing
+interface printTeacherFunction {
+    firstName: string;
+    lastName: string;
+}
+
+function printTeacher(input: printTeacherFunction) : string {
+    return `${input.firstName[0]}. ${input.lastName}`
+}
+
+const teacher: printTeacherFunction = {
+    firstName: "John",
+    lastName: "Doe"
+}
+
+console.log(printTeacher(teacher));
+
+interface studentInterface{
+    firstName:string;
+    lastName:string;
+    workOnHomework(): string;
+    displayName():string;
+}
+
+class StudentClass implements studentInterface{
+    firstName: string;
+    lastName:string;
+    constructor(firstName:string, lastName:string){
+       this.firstName = firstName,
+       this.lastName = lastName
+    }
+    workOnHomework(): string {
+        return "Currently working"
+    }
+    displayName(): string {
+        return this.firstName
+    }
+}
