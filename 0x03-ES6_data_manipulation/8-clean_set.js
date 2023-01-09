cb37ccd30bc2eced83d955f83020612de8729b15
melakualehegn <melakualehegn34@gmail.com>
2023-01-09 15:43:16 +0300

    task-8 es fix

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,16 +1,16 @@
 export default function cleanSet(set, startString) {
-    const parts = [];
-    if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
-      return '';
-    }
-    for (const value of set.values()) {
-      if (typeof value === 'string' && value.startsWith(startString)) {
-        const valueStr = value.substring(startString.length);
-  
-        if (valueStr && valueStr !== value) {
-          parts.push(valueStr);
-        }
+  const parts = [];
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+    return '';
+  }
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const valueStr = value.substring(startString.length);
+
+      if (valueStr && valueStr !== value) {
+        parts.push(valueStr);
       }
     }
-    return parts.join('-');
   }
+  return parts.join('-');
+}
